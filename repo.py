d1c391b16ffc76d2830eccb9773bc511249c7b00
Jake Glenn <jbradleyglenn@gmail.com>
2022-08-05 17:02:12 -0400

    added project

---
+++ repo.py
@@ -0,0 +1,52 @@
+# imports
+import configparser
+
+# internal representation of a repo 
+class Repo:
+  workdir = None
+  gitdir = None
+  config = None
+
+  def __init__(self, workdir):
+    self.workdir = workdir
+    self.gitdir = os.path.join(workdir, ".git")
+    self.config = configparser.ConfigParser()
+
+    if not os.path.isdir(self.gitdir):
+      raise Exception("Not a git repository %s" % workdir)
+
+    configpath = repo_path(self, "config")
+    if os.path.exists(repo_path):
+      self.config.read(repo_path)
+    else:
+      raise Exception("No config for git repository %s" % workdir)
+
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_find(path="."):
+  path = os.path.realpath(path)
+  if os.path.isdir(os.path.join(path, ".git")):
+    return Repo(path)
+  else:
+    parent = os.path.realpath(os.path.join(path, ".."))
+    if parent == path:
+      raise Exception("No repository")
+    return repo_find(parent)
---
+++ tinygit.py
@@ -0,0 +1,94 @@
+# imports
+import sys
+
+import os
+
+import argparse
+
+import configparser
+
+# parser to parse command line arguments 
+
+parser = argparse.ArgumentParser()
+subs = parser.add_subparsers(title="commands", dest="command", required=True)
+
+sp = subs.add_parser("init", help="Initialize a new, empty repository.")
+sp.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository.")
+
+sp = subs.add_parser("cat-file", help="Provide content of repository objects")
+sp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"], help="Specify the type")
+sp.add_argument("object", metavar="object", help="The object to display")
+
+sp = subs.add_parser("log", help="Display history of a given commit.")
+sp.add_argument("commit", default="HEAD", nargs="?", help="Commit to start at.")
+
+sp = subs.add_parser("ls-tree", help="Pretty-print a tree object.")
+sp.add_argument("object", help="The object to show.")
+
+sp = subs.add_parser("checkout", help="Checkout a commit inside of a directory.")
+sp.add_argument("commit", help="The commit or tree to checkout.")
+sp.add_argument("path", help="The EMPTY directory to checkout on.")
+
+sp = subs.add_parser("show-ref", help="List references.")
+
+sp = subs.add_parser("tag", help="List and create tags")
+sp.add_argument("-a", action="store_true", dest="create_tag_object", help="Whether to create a tag object")
+sp.add_argument("name", nargs="?", help="The new tag's name")
+sp.add_argument("object", default="HEAD", nargs="?", help="The object the new tag will point to")
+
+sp = subs.add_parser("rev-parse", help="Parse revision (or other objects )identifiers")
+sp.add_argument("--wyag-type", metavar="type", dest="type", choices=["blob", "commit", "tag", "tree"], default=None, help="Specify the expected type")
+sp.add_argument("name", help="The name to parse")
+
+# main entry point
+
+def main():
+  args = parser.parse_args(sys.argv[1:])  
+  if   args.command == "add"         : cmd_add(args)
+  elif args.command == "cat-file"    : cmd_cat_file(args)
+  elif args.command == "checkout"    : cmd_checkout(args)
+  elif args.command == "commit"      : cmd_commit(args)
+  elif args.command == "hash-object" : cmd_hash_object(args)
+  elif args.command == "init"        : cmd_init(args)
+  elif args.command == "log"         : cmd_log(args)
+  elif args.command == "ls-tree"     : cmd_ls_tree(args)
+  elif args.command == "merge"       : cmd_merge(args)
+  elif args.command == "rebase"      : cmd_rebase(args)
+  elif args.command == "rev-parse"   : cmd_rev_parse(args)
+  elif args.command == "rm"          : cmd_rm(args)
+  elif args.command == "show-ref"    : cmd_show_ref(args)
+  elif args.command == "tag"         : cmd_tag(args)
+
+# commands
+
+def cmd_init(args):
+  """args.path must either not exist or be empty"""
+  workdir = args.path
+
+  if not os.path.exists(workdir):
+    os.makedirs(workdir)
+  else:
+    if not os.path.isdir(workdir):
+      raise Exception("workdir is not a directory")
+    elif os.listdir(workdir):
+      raise Exception("workdir is not empty")
+      
+  os.makedirs(os.path.join(workdir, ".git", "branches"))
+  os.makedirs(os.path.join(workdir, ".git", "objects"))
+  os.makedirs(os.path.join(workdir, ".git", "refs", "tags"))
+  os.makedirs(os.path.join(workdir, ".git", "refs", "heads"))
+
+  with open(os.path.join(workdir, ".git", "description"), "w") as f:
+    f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+  with open(os.path.join(workdir, ".git", "HEAD"), "w") as f:
+    f.write("ref: refs/heads/master\n")
+
+  with open(os.path.join(workdir, ".git", "config"), "w") as f:
+    dconfig = configparser.ConfigParser()
+    dconfig['core'] = {"repositoryformatversion": "0", "filemode": "false", "bare": "false"}
+    dconfig.write(f)
+
+def cmd_
+    
+
