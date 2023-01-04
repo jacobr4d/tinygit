447db18d15d1f9facd044c7e38d7195b58e05207
Jake Glenn <jbradleyglenn@gmail.com>
2023-01-03 20:04:33 -0500

    added HEAD tests to test_commit

--- test/test_commit.py
+++ test/test_commit.py
@@ -19,11 +19,11 @@ class TestInit(unittest.TestCase):
         out = subprocess.run(["tinygit", "commit", "this wont' work"], capture_output=True)
         self.assertEqual(out.returncode, 1)
 
-    def test_commit_workdir_empty_runs(self):
+    def test_commit_empty_runs(self):
         os.system("tinygit init >> /dev/null")
         os.system("tinygit commit 'first commit' >> /dev/null")
 
-    def test_commit_workdir_empty_commit_stored(self):
+    def test_commit_empty_commit_stored(self):
         os.system("tinygit init >> /dev/null")
         out = subprocess.run(["tinygit", "commit", "message"], capture_output=True).stdout.decode()
         commitsha = out.split()[1]
@@ -32,7 +32,7 @@ class TestInit(unittest.TestCase):
             b'{\n  "headers": {\n    "tree": "01704555d3be59fda548e5524445e25b07c9b509"\n  },\n  "body": "message"\n}'
         )
 
-    def test_commit_workdir_empty_tree_stored(self):
+    def test_commit_empty_tree_stored(self):
         os.system("tinygit init >> /dev/null")
         out = subprocess.run(["tinygit", "commit", "message"], capture_output=True).stdout.decode()
         commitsha = out.split()[1]
@@ -55,7 +55,37 @@ class TestInit(unittest.TestCase):
             b'aaaaa\n'
         )
 
-    # test commit branch
+    def test_head_after_commit_branch(self):
+        os.system("tinygit init >> /dev/null")
+        os.system("tinygit commit first >> /dev/null")
+        self.assertEqual(
+            repo_find().get_head(),
+            {
+                "type": "branch",
+                "id": "master"
+            }
+        )
+        self.assertEqual(
+            repo_find().resolve_head(),
+            "3e1a939bd3fd30ba3d114bedba0fb888183f0b20"
+        )
+
+    def test_head_after_commit_detached(self):
+        os.system("tinygit init >> /dev/null")
+        os.system("tinygit commit first >> /dev/null")
+        os.system("tinygit checkout-commit HEAD >> /dev/null")
+        self.assertEqual(
+            repo_find().get_head(),
+            {
+                "type": "commit",
+                "id": "3e1a939bd3fd30ba3d114bedba0fb888183f0b20"
+            }
+        )
+        self.assertEqual(
+            repo_find().resolve_head(),
+            "3e1a939bd3fd30ba3d114bedba0fb888183f0b20"
+        )
+
 
     # test commit detached
 
